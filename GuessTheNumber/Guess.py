4ef36ea640c59f0dfec9b1ade9e39274254e85a7
Hilal Morrar <hamorrar@ucsc.edu>
2020-03-20 23:39:07 -0700

    wokring on making it play again:

--- GuessTheNumber/Guess.py
+++ GuessTheNumber/Guess.py
@@ -1,1 +1,14 @@
-print('Hello World!')
+import random
+import sys
+
+def main():
+    CPUNum = rand.randint(1, 21)
+    print("Guess a number between 1 and 20: ")
+    userGuess = input()
+    if userGuess == CPUNum:
+        print("Correct! Want to play again? (Y/N)")
+        playAgain = input();
+        if playAgain == 'Y':
+            main()
+        elif playAgain == 'N':
+            print("Thank you!")
