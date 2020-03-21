31b044fb937f601bc77fb90b26047eb034054d59
Hilal Morrar <hamorrar@ucsc.edu>
2020-03-21 00:02:19 -0700

    fixed some sytax bugs

--- GuessTheNumber/Guess.py
+++ GuessTheNumber/Guess.py
@@ -1,29 +1,31 @@
-import random
+import random as rand
 import sys
 
 def main():
     CPUNum = rand.randint(1, 21)
     print("Guess a number between 1 and 20: ")
-    userGuess = input()
+    userGuess = int(input())
     while(userGuess != CPUNum):
         if userGuess < CPUNum:
             print("Higher!")
-            userGuess = input()
+            userGuess = int(input())
         else:
             print("Lower!")
-            userGuess = input()
+            userGuess = int(input())
     if userGuess == CPUNum:
         print("Correct! Want to play again? (Y/N)")
         again = input()
         playAgainCheck(again)
 
 def playAgainCheck(userAgain):
-    if playAgain == 'Y':
+    if userAgain == 'Y' or userAgain == 'y':
         main()
-    elif playAgain == 'N':
+    elif (userAgain == 'N') or (userAgain == 'n'):
         print("Thank you!")
         sys.exit()
     else:
         print("Sorry I couldn't understand. Play again? (Y/N)")
-        again = input()
-        playAgainCheck(again)s
+        userAgain = input()
+        playAgainCheck(user)
+
+main()
