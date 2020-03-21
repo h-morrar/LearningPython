4f831756a479f819915d216b9a805ab80a51fc6c
Hilal Morrar <hamorrar@ucsc.edu>
2020-03-20 23:48:51 -0700

    added loop to keep checking input

--- GuessTheNumber/Guess.py
+++ GuessTheNumber/Guess.py
@@ -5,10 +5,25 @@ def main():
     CPUNum = rand.randint(1, 21)
     print("Guess a number between 1 and 20: ")
     userGuess = input()
+    while(userGuess != CPUNum):
+        if userGuess < CPUNum:
+            print("Higher!")
+            userGuess = input()
+        else:
+            print("Lower!")
+            userGuess = input()
     if userGuess == CPUNum:
         print("Correct! Want to play again? (Y/N)")
-        playAgain = input();
-        if playAgain == 'Y':
-            main()
-        elif playAgain == 'N':
-            print("Thank you!")
+        again = input()
+        playAgainCheck(again)
+
+def playAgainCheck(userAgain):
+    if playAgain == 'Y':
+        main()
+    elif playAgain == 'N':
+        print("Thank you!")
+        sys.exit()
+    else:
+        print("Sorry I couldn't understand. Play again? (Y/N)")
+        again = input()
+        playAgainCheck(again)s
